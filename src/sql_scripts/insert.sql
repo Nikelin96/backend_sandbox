41bef8b1b5c5c1ba7cf8339ed9f8f4896e636796
mykola <nikelinchik@gmail.com>
2023-06-30 15:45:39 +0300

    Fixed issues, improved sql scripts

--- src/sql_scripts/insert.sql
+++ src/sql_scripts/insert.sql
@@ -1,59 +1,23 @@
+BEGIN;
+
 -- create Spearmen with Spear
+INSERT INTO storage (type, food, gold, wood) VALUES ('expense', 5, 10, NULL); -- id 1 for spearmen
+INSERT INTO stat (hit_points, damage_points) VALUES (100, 1); -- id 1 for spearmen
+INSERT INTO unit (name, stat_id, storage_id) VALUES ('Spearmen', 1, 1); -- id 1
 
-INSERT INTO 
-    storage(food, gold, wood)
-VALUES
-    (5, 10, NULL), -- for spearmen
-    (NULL, NULL, 5); -- for spear
-
-INSERT INTO 
-    stat(hit_points, damage_points)
-VALUES
-    (100, 1), -- for spearmen
-    (0, 10); -- for spear
-
-INSERT INTO 
-    unit(name, stat_id, storage_id)
-VALUES 
-	('Spearmen', 1, 1); -- spearmen
-
-INSERT INTO 
-    equipment(name, stat_id, storage_id, unit_id)
-VALUES 
-	('Spear', 2, 2, 1); -- spear
+INSERT INTO storage (type, food, gold, wood) VALUES ('expense', NULL, NULL, 5); -- id 2 for spear
+INSERT INTO stat (hit_points, damage_points) VALUES (0, 10); -- id 2 for spear
+INSERT INTO equipment (name, stat_id, storage_id, unit_id) VALUES ('Spear', 2, 2, 1); -- id 1 for Spear, referencing unit id 1
 
 -- create Market with Mail armor
-INSERT INTO 
-    storage(wood, food, gold, stone)
-VALUES
-    (100, 100, 100, 100); -- for market
+INSERT INTO storage (type, wood, food, gold, stone) VALUES ('income', 100, 100, 100, 100); -- id 3 for market
+INSERT INTO stat (hit_points, defense_points) VALUES (100, 1); -- id 3 for market
+INSERT INTO unit (name, stat_id, storage_id) VALUES ('Market', 3, 3); -- id 2 for market
 
-INSERT INTO 
-    stat(hit_points, defense_points)
-VALUES
-    (100, 1); -- for market
+INSERT INTO market (tax, unit_id) VALUES (30, 2); -- referencing unit id 2 for market
 
-INSERT INTO 
-    unit(name, stat_id, storage_id)
-VALUES 
-	('Market', 3, 3); -- market
+INSERT INTO storage (type, gold) VALUES ('expense', 5); -- id 4 for mail
+INSERT INTO stat (hit_points, defense_points) VALUES (10, 2); -- id 4 for mail
+INSERT INTO equipment (name, stat_id, storage_id, unit_id) VALUES ('Mail armor', 4, 4, 2); -- referencing unit id 2 for market
 
-INSERT INTO
-    market(tax, unit_id)
-VALUES
-    (30, 2); -- market
-
-INSERT INTO 
-    storage(gold)
-VALUES
-    (5); -- for mail
-
-INSERT INTO 
-    stat(hit_points, defense_points)
-VALUES
-    (10, 2); -- for mail
-
-INSERT INTO
-    equipment(name, stat_id, storage_id, unit_id)
-VALUES
-    ('Mail armor', 4, 4, 2) -- mail
+COMMIT;
