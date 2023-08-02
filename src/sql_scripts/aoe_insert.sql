be5912e2418b1694ff9170dcdefcc2e82ac3ed24
mrevutskyi <mrevutskyi@brightgrove.com>
2023-08-02 16:03:59 +0300

    added  equipment insert as required technology

--- src/sql_scripts/aoe_insert.sql
+++ src/sql_scripts/aoe_insert.sql
@@ -26,13 +26,24 @@ INSERT INTO technology_dependency (technology_id, is_required, unit_id, skill_id
 
 -- Insert technology(Skill -> attack)
 INSERT INTO technology (name, description, research_time) VALUES ('attack technology', 'A technology for attacking', 50);
-INSERT INTO price(wood, food, gold, stone, technology_id, unit_id, skill_id) VALUES (10, 10, 10, 10, 2, null, null);
+INSERT INTO price(wood, food, gold, stone, technology_id, unit_id, skill_id, equipment_id) VALUES (10, 10, 10, 10, 2, null, null, null);
 -- Insert Skill(attack)
 INSERT INTO stat (hit_points) VALUES (10);
 INSERT INTO skill (type, stat_id) VALUES ('attack', 2); 
-INSERT INTO price(wood, food, gold, stone, technology_id, unit_id, skill_id) VALUES (5, 5, 5, 5, null, null, 1);
+INSERT INTO price(wood, food, gold, stone, technology_id, unit_id, skill_id, equipment_id) VALUES (4, 4, 4, 4, null, null, 1, null);
 -- Insert technology dependencies: Technology(attak) --> Skill(attack)
 INSERT INTO technology_dependency (technology_id, is_required, unit_id, skill_id) VALUES(2, true, null, 1);
+
+
+-- Insert technology(Equipment -> chain mail)
+INSERT INTO technology (name, description, research_time) VALUES ('chain mail', 'A technology for chain mail', 50);
+INSERT INTO price(wood, food, gold, stone, technology_id, unit_id, skill_id, equipment_id) VALUES (10, 10, 10, 10, 3, null, null, null);
+-- Insert Equipment(chain mail)
+INSERT INTO stat (health_points, defense_points) VALUES (10, 2);
+INSERT INTO equipment (name, stat_id) VALUES ('chain mail', 3); 
+INSERT INTO price(wood, food, gold, stone, technology_id, unit_id, skill_id, equipment_id) VALUES (3, 3, 3, 3, null, null, null, 1);
+-- Insert technology dependencies: Technology(attak) --> Skill(attack)
+INSERT INTO technology_dependency (technology_id, is_required, unit_id, skill_id, equipment_id) VALUES(3, true, null, null, 1);
 
 
 -- Insert transaction: Kingdom -> expense -> Technology(Unit -> Spearmen)
@@ -45,16 +56,25 @@ INSERT INTO kingdom_transaction (kingdom_id, type, wood, food, gold, stone) VALU
 INSERT INTO kingdom_technology (kingdom_id, technology_id, kingdom_transaction_id, research_status, research_start_time) VALUES(1, 2, 3, 'completed', CURRENT_TIMESTAMP - INTERVAL '30 days');
 
 
+-- Insert transaction: Kingdom -> expense -> Technology(Equipment -> chain mail)
+INSERT INTO kingdom_transaction (kingdom_id, type, wood, food, gold, stone) VALUES (1, 'expense', 10, 10, 10, 10);
+INSERT INTO kingdom_technology (kingdom_id, technology_id, kingdom_transaction_id, research_status, research_start_time) VALUES(1, 2, 4, 'completed', CURRENT_TIMESTAMP - INTERVAL '30 days');
+
+
 -- Insert transaction: Kingdom -> expense -> Unit(Spearmen)
 INSERT INTO kingdom_transaction (kingdom_id, type, wood, food, gold, stone) VALUES (1, 'expense', 5, 5, 5, 5);
--- Insert transaction: Kingdom -> expense -> Skill(Attack)
+-- Insert transaction: Kingdom -> expense -> Skill(attack)
 INSERT INTO kingdom_transaction (kingdom_id, type, wood, food, gold, stone) VALUES (1, 'expense', 4, 4, 4, 4);
+-- Insert transaction: Kingdom -> expense -> Equipment(chain mail)
+INSERT INTO kingdom_transaction (kingdom_id, type, wood, food, gold, stone) VALUES (1, 'expense', 3, 3, 3, 3);
 
 
 -- Attach Unit(Spearmen) --> Kingdom(England)
 INSERT INTO kingdom_unit (kingdom_id, unit_id, kingdom_transaction_id) VALUES (1, 1, 4);
 -- Attach Skill(attack) --> Unit(Spearmen) --> Kingdom(England)
 INSERT INTO unit_skill (skill_id, unit_id, kingdom_transaction_id) VALUES (1, 1, 5);
+-- Attach Equipment(chain mail) --> Unit(Spearmen) --> Kingdom(England)
+INSERT INTO unit_equipment (equipment_id, unit_id, kingdom_transaction_id) VALUES (1, 1, 6);
 
 
 COMMIT;
