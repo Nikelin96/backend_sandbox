348394124b5ae484594cadf686129ce4e86dbacb
mrevutskyi <mrevutskyi@brightgrove.com>
2023-08-02 18:43:23 +0300

    added console helper for  automation purposes

---
+++ src/dotnet_microservices/console_helper/ConsoleHelper/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleHelper;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+
+
+var configuraion = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false).Build();
+
+var address = configuraion.GetConnectionString("TargetServer");
+using var channel = GrpcChannel.ForAddress(address);
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                 new HelloRequest { Name = "Nik" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
