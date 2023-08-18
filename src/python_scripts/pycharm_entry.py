620e30d5b4918eb1e3485809b24e098a83303614
mrevutskyi <mrevutskyi@brightgrove.com>
2023-08-18 16:42:19 +0300

    Updated python playground

--- src/python_scripts/pycharm_entry.py
+++ src/python_scripts/pycharm_entry.py
@@ -1,23 +1,13 @@
-# This is a sample Python script.
+class Point:
+    def Move(self):
+        print("mov3")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-patientName, age, s = ["john", 23, False]
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-print_hi('Py   Charm ')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-name = input("What is your name ?")
-print("Hello " + name)
+    def Draw(self):
+        print("draw")
 
 
 
+p1 = Point()
+p1.Draw()
+p1.w = "asd"
+print(p1.w)
