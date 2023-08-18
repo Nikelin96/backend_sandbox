16411f78693fe6d18921b83a2d7c65952e2813f2
mrevutskyi <mrevutskyi@brightgrove.com>
2023-08-18 07:44:57 +0300

    updated python

--- src/python_scripts/hello_world.py
+++ src/python_scripts/hello_world.py
@@ -1,1 +1,16 @@
-print('Hello, World!')
+# # birth_year = input("Enter your birth year: ")
+# # age = 2020 - int(birth_year)
+# # print(age)
+
+
+# first = float(input("Enter your first number: "))
+
+# second = float(input("Enter second number: "))
+
+# print("Sum : " + str(first+second))
+
+
+numbers = [0, 2, 3, 4, 5, 6]
+
+numbers.insert(-1, 7)
+print(numbers)
---
+++ src/python_scripts/pycharm_entry.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+patientName, age, s = ["john", 23, False]
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+print_hi('Py   Charm ')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+name = input("What is your name ?")
+print("Hello " + name)
+
+
+
