72852f75198cc204daaed79b6431278d6a86fc68
mrevutskyi <mrevutskyi@brightgrove.com>
2023-08-18 11:36:48 +0300

    played with python

--- src/python_scripts/hello_world.py
+++ src/python_scripts/hello_world.py
@@ -1,16 +1,28 @@
-# # birth_year = input("Enter your birth year: ")
-# # age = 2020 - int(birth_year)
-# # print(age)
-
-
-# first = float(input("Enter your first number: "))
-
-# second = float(input("Enter second number: "))
-
-# print("Sum : " + str(first+second))
-
-
-numbers = [0, 2, 3, 4, 5, 6]
-
-numbers.insert(-1, 7)
-print(numbers)
+command = ""
+isStarted = False
+while (True):
+    command = input('> ')
+    if command.find('help') > -1:
+        text = '''
+start - to start the car
+stop - to stop the car
+quit - to exit
+        '''
+        print(text)
+    elif command.find('start') > -1:
+        if isStarted == True:
+            print("car is already started!")
+        else:
+            print("started car")
+        isStarted = True
+    elif command.find('stop') > -1:
+        if isStarted == False:
+            print("car is already stopped!")
+        else:
+            print("stopped car")
+        isStarted = False
+    elif command.find('quit') > -1:
+        print("quitting the car..")
+    else:
+        print("I don't understand that")
+    previousCommand = command
