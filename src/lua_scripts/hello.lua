0e9eb4f42b8e115527d54385c11a6e543014c616
mrevutskyi <nikelinchik@gmail.com>
2024-03-23 14:56:24 +0200

    improved lua script

--- src/lua_scripts/hello.lua
+++ src/lua_scripts/hello.lua
@@ -1,2 +1,16 @@
+local function readFromFile(path)
+    local file = io.open(path, "r")
+    if file then
+        local content = file:read("*a")
+        file:close()
+        return content
+    else
+        return nil
+    end
+end
+
 local hello = "Hi Mom"
 print(hello)
+local res = readFromFile('input.txt')
+print(res)
+
